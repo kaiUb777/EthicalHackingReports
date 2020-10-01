8ef914912ea44ab4c32b2fea5b130ff82ea25c5a
kaiUb <1140396@isep.ipp.pt>
2020-10-01 11:35:15 +0100

    Add reports + corretion reports

--- HackTheBox/Challenges/Mobile/Cryptohorrific/hackthebox.app/decoder.py
+++
@@ -1,5 +0,0 @@
-import base64
-decoded_data = base64.b64decode("Tq+CWzQS0wYzs2rJ+GNrPLP6qekDbwze6fIeRRwBK2WXHOhba7WR2OGNUFKoAvyW7njTCMlQzlwIRdJvaP2iYQ==")
-
-print("decoded text is ")
-print(decoded_data)
